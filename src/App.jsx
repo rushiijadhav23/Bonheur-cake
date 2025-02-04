71b4fb743315dd8fa74c267040231617d897c57e
aniketmdinde <aniketmdinde@gmail.com>
2025-02-04 23:38:33 +0530

    Dripping icing backjgroynd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Hero from "./components/Hero"
-// import './App.css'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div className="w-full">
       <Hero />
-    </>
+    </div>
   )
 }
 
